c0d44395f31d3960e3dabbf0877831e42e71f924
vcbartolome <vcbartolome@up.edu.ph>
2026-06-24 08:30:12 +0800

    fix (role): import access to admin agtech

--- app/(user)/import/layout.tsx
+++ app/(user)/import/layout.tsx
@@ -1,12 +1,13 @@
 import { ProtectedRoute } from "@/components/protected-route";
 
+
 export default function ImportLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <ProtectedRoute requireApproval={true} requiredRole="admin">
+    <ProtectedRoute requireApproval={true} requiredRole={["admin","agtech"]}>
       {children}
     </ProtectedRoute>
   );
